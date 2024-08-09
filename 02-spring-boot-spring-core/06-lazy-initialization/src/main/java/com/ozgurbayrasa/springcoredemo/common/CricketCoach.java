7fe062ab9c191e78f66afc3fd9d9017289a64ceb
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-08-09 16:21:33 +0300

    Lazy Initialization Demo Added.

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/ozgurbayrasa/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/ozgurbayrasa/springcoredemo/common/CricketCoach.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Component;
 // for injection.
 @Component
 public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
-        return "Practise fast bowling for 20 minutes.";
+        return "Practise fast bowling for 15 minutes.";
     }
 }
