7d78b9d46a1c0341a5f861ddf8482f298ed17c0c
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-08-16 06:46:37 +0300

    Employee Crud App - Implementing delete method.

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ozgurbayrasa/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ozgurbayrasa/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,10 +2,7 @@ package com.ozgurbayrasa.springboot.cruddemo.rest;
 
 import com.ozgurbayrasa.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ozgurbayrasa.springboot.cruddemo.service.EmployeeService;
 
 import java.util.List;
@@ -38,5 +35,36 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
+    }
+
+    // Add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        // Set id to 0 to make a save instead of update.
+        theEmployee.setId(0);
+
+        Employee employeeNew = employeeService.save(theEmployee);
+        return employeeNew;
+    }
+
+    // Add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee employeeUpdated = employeeService.save(theEmployee);
+        return employeeUpdated;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee is not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Employee deleted with id - " + employeeId;
     }
 }
