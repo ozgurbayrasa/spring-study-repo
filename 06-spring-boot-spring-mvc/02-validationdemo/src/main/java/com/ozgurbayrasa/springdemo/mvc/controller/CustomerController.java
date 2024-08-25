95cce8636197086ab6b29eed16e5f7a60c5b6dee
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-08-25 05:57:02 +0300

    Add Validation Regex, Number Range and Required with Init Binder

--- 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/ozgurbayrasa/springdemo/mvc/controller/CustomerController.java
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/ozgurbayrasa/springdemo/mvc/controller/CustomerController.java
@@ -2,15 +2,28 @@ package com.ozgurbayrasa.springdemo.mvc.controller;
 
 import com.ozgurbayrasa.springdemo.mvc.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // Add an init binder to convert trim input strings.
+    // Remove leading and trailing whitespace.
+    // Resolve issue in validation.
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -24,6 +37,9 @@ public class CustomerController {
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
                               BindingResult bindingResult){
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+
         if(bindingResult.hasErrors()){
             return "customer-form";
         }
