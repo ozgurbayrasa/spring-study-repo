89b97daa565b7caa5544f27317e7fac1beb55ee4
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-08-25 05:11:20 +0300

    Add Customer as Model and Controller

---
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/ozgurbayrasa/springdemo/mvc/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.ozgurbayrasa.springdemo.mvc.controller;
+
+import com.ozgurbayrasa.springdemo.mvc.model.Customer;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel){
+
+        // Add customer as attribute to the model.
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "customer-form";
+        }
+        else{
+            return "customer-confirmation";
+        }
+    }
+}
