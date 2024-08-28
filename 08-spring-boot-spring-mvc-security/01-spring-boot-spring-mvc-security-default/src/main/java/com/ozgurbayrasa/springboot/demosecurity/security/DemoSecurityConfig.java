c64ffb80d7797e29afddf0f3e503952e683e2590
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-08-28 08:05:58 +0300

    Logout and Login Spring Security

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/ozgurbayrasa/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.ozgurbayrasa.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated())
+                .formLogin(form ->
+                        form
+                                // Which login page to show?
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                // Every user can see login form.
+                                .permitAll())
+                // Default URL -> /logout
+                .logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+}
